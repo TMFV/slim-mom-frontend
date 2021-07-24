b981e70a124c45616a4fdb5447cf6bcf7a29dec0
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-24 21:33:58 +0300

    link for homepage in logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { ReactComponent as LogoImage } from '../../assets/pictures/logo.svg';
 import { ReactComponent as LogoText } from '../../assets/pictures/logo-text.svg';
 import styles from './Logo.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Logo({ isAuthorized = false }) {
   return (
-    <div>
+    <Link className={styles.logowrapper} to="/">
       <LogoImage width={46} height={44} />
       {!isAuthorized && <LogoText width={107} height={16} />}
-    </div>
+    </Link>
   );
 }
