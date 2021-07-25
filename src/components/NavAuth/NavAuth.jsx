cf9905aae810cf3df0632649cee5c127bf994a63
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-25 16:39:47 +0300

    auth logic for classnames

--- src/components/NavAuth/NavAuth.jsx
+++ src/components/NavAuth/NavAuth.jsx
@@ -1,3 +1,22 @@
+import { NavLink } from 'react-router-dom';
+import styles from './NavAuth.module.scss';
 export default function NavAuth() {
-  return <div>navauth</div>;
+  return (
+    <div className={styles.NavNotAuthWrapper}>
+      <NavLink
+        to="/diary"
+        className={styles.enter}
+        activeClassName={styles.activeEnter}
+      >
+        дневник
+      </NavLink>
+      <NavLink
+        to="/calculator"
+        className={styles.enter}
+        activeClassName={styles.activeEnter}
+      >
+        калькулятор
+      </NavLink>
+    </div>
+  );
 }
