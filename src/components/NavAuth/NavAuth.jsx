8cbb8563d10425cb2cc9c1ba51427d99f32f3b59
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-25 18:01:35 +0300

    auth header mobile done

--- src/components/NavAuth/NavAuth.jsx
+++ src/components/NavAuth/NavAuth.jsx
@@ -1,22 +1,47 @@
 import { NavLink } from 'react-router-dom';
+// import {
+//   authSelectors,
+//   authOperations,
+// } from '../../redux/auth/auth-operations';
+import { useSelector, useDispatch } from 'react-redux';
+import { useCallback } from 'react';
 import styles from './NavAuth.module.scss';
+
 export default function NavAuth() {
+  const dispatch = useDispatch();
+  // const name = useSelector(authSelectors.getUsername);
+  const name = 'CurrentUser';
+
+  // const onLogOut = useCallback(() => {
+  //   dispatch(authOperations.logOut());
+  // }, [dispatch]);
+  const onLogOut = console.log('logout');
   return (
-    <div className={styles.NavNotAuthWrapper}>
-      <NavLink
-        to="/diary"
-        className={styles.enter}
-        activeClassName={styles.activeEnter}
-      >
-        дневник
-      </NavLink>
-      <NavLink
-        to="/calculator"
-        className={styles.enter}
-        activeClassName={styles.activeEnter}
-      >
-        калькулятор
-      </NavLink>
+    <div className={styles.NavAuthWrapper}>
+      <div className={styles.linksWapper}>
+        <NavLink
+          to="/diary"
+          className={styles.enter}
+          activeClassName={styles.activeEnter}
+        >
+          дневник
+        </NavLink>
+        <NavLink
+          to="/calculator"
+          className={styles.enter}
+          activeClassName={styles.activeEnter}
+        >
+          калькулятор
+        </NavLink>
+      </div>
+
+      <button>burger</button>
+      <div className={styles.userWrapper}>
+        <p className={styles.userName}>{name}</p>
+        <button onClick={onLogOut} className={styles.logout}>
+          Выйти
+        </button>
+      </div>
     </div>
   );
 }
