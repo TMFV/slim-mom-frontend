7148dfebcdba5e335c6d908de4f88d52ee56ae54
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-25 18:36:23 +0300

    stub for form

---
+++ src/components/FormUser/FormUser.jsx
@@ -0,0 +1,9 @@
+import styles from './FormUser.module.scss';
+
+export default function FormUser() {
+  return (
+    <div className={styles.formWrapper}>
+      <h1>Просчитай свою суточную норму калорий прямо сейчас</h1>
+    </div>
+  );
+}
