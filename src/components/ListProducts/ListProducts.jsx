31a7d60dda89c40fcd64fda10cddb637f918cc3c
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-07-27 18:47:28 +0800

    Update ListProducts.jsx

--- src/components/ListProducts/ListProducts.jsx
+++ src/components/ListProducts/ListProducts.jsx
@@ -2,8 +2,8 @@ import ItemProduct from '../ItemProduct';
 import { productsSelectors } from '../../redux/products';
 import style from './ListProduct.module.scss';
 import { useSelector } from 'react-redux';
-import SimpleBar from 'simplebar';
-import 'simplebar/dist/simplebar.min.css';
+// import SimpleBar from 'simplebar';
+// import 'simplebar/dist/simplebar.min.css';
 
 const ListProducts = () => {
   const listProducts = useSelector(productsSelectors.getStateProducts);
